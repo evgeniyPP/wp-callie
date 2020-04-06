662f190acbd8a05a200ade2b4f0844a2240c03d2
evgeniyPP <amenat12@mail.ru>
2020-04-06 22:54:49 +0300

    All 'ContactMe Form' logic has been moved to a custom plugin

--- functions.php
+++ functions.php
@@ -150,7 +150,6 @@ add_action('init', function () {
         'hierarchical'        => false,
         'supports'            => ['title', 'editor', 'thumbnail'],
         'has_archive'         => true
-        // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
     ]);
 
     register_taxonomy('city', ['jobs'], [
@@ -169,29 +168,6 @@ add_action('init', function () {
         'description'           => '',
         'public'                => true,
         'hierarchical'          => false
-    ]);
-
-    register_post_type('messages', [
-        'labels' => array(
-            'name'               => 'Сообщения',
-            'singular_name'      => 'Сообщение',
-            'add_new'            => 'Добавить новое',
-            'add_new_item'       => 'Добавление сообщения',
-            'edit_item'          => 'Редактирование сообщения',
-            'new_item'           => 'Новое сообщение',
-            'view_item'          => 'Смотреть сообщение',
-            'search_items'       => 'Искать сообщение',
-            'not_found'          => 'Не найдено',
-            'not_found_in_trash' => 'Не найдено в корзине',
-            'parent_item_colon'  => '',
-            'menu_name'          => 'Сообщения',
-        ),
-        'description'         => '',
-        'public'              => true,
-        'menu_position'       => 25,
-        'menu_icon'           => 'dashicons-email-alt2',
-        'hierarchical'        => false,
-        'supports'            => ['title', 'editor']
     ]);
 });
 
@@ -239,62 +215,3 @@ add_shortcode('callie_contactme_form', function () {
 
 
 add_image_size('jobs-thumb', 440, 85, true);
-
-add_action('wp_footer', function () {
-    // Send backend vars to frontend
-    $vars = [
-        'templateUrl' => get_template_directory_uri(),
-        'ajaxUrl' => admin_url('admin-ajax.php')
-    ];
-
-    echo '<script>window.wp = ' . json_encode($vars) . '</script>';
-});
-
-add_action('wp_ajax_contactme', 'callie_ajax_contactme');
-add_action('wp_ajax_nopriv_contactme', 'callie_ajax_contactme');
-
-function callie_ajax_contactme()
-{
-    $name = htmlspecialchars(trim($_POST['name']));
-    $email = htmlspecialchars(trim($_POST['email']));
-    $subject = htmlspecialchars(trim($_POST['subject']));
-    $message = htmlspecialchars(trim($_POST['message']));
-
-    if ($name == null || $email == null || $subject == null || $message == null) {
-        $res = [
-            'success' => false,
-            'errors' => ['Данные некорретны']
-        ];
-    } else {
-        $post_data = array(
-            'post_type' => 'messages',
-            'post_title'    => $subject,
-            'post_content'  => $message,
-            'post_status'   => 'publish'
-        );
-
-        $new_post_id = wp_insert_post(wp_slash($post_data));
-
-        $new_post_id = CFS()->save([
-            'name' => $name,
-            'email' => $email
-        ], [
-            'ID' => $new_post_id
-        ]);
-
-        if (!$new_post_id) {
-            $res = [
-                'success' => false,
-                'errors' => ['Ошибка при внесении в БД']
-            ];
-        } else {
-            $res = [
-                'success' => true,
-                'errors' => []
-            ];
-        }
-    }
-
-    echo json_encode($res);
-    wp_die();
-}
