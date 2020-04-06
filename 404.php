022048debaf101809de230fa1924531b1882e376
evgeniyPP <amenat12@mail.ru>
2020-04-06 14:02:23 +0300

    Error 404

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <div class="col-md-8 error404">
+                <h1>Ошибка 404. Страница не найдена</h1>
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+
+<?php get_footer(); ?>
